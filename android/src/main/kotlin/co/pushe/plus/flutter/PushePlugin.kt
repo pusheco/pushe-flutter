53629393284d21d82e7d03bf84fa4dcbae32e820
Mahdi Malvandi <mmalvandi75@gmail.com>
2020-02-15 12:33:27 +0330

    Feat: Recreate plugin using EmbeddedV2
    Changes:
    
    - Use Kotlin instead of Java
    - Migrate to Android Embedded v2
    - Support Background callbacks
    - APIs for NotificationChannel and CustomSound
    - Fix and Improve Event, etc
    - Update sample of Pushe
    - Update version
    - Minor fixed and improvements

---
+++ android/src/main/kotlin/co/pushe/plus/flutter/PushePlugin.kt
@@ -0,0 +1,118 @@
+package co.pushe.plus.flutter
+
+import android.content.Context
+import android.util.Log
+import co.pushe.plus.flutter.Utils.lg
+import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.embedding.engine.plugins.FlutterPlugin.FlutterPluginBinding
+import io.flutter.embedding.engine.plugins.activity.ActivityAware
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
+import io.flutter.plugin.common.BinaryMessenger
+import io.flutter.plugin.common.MethodChannel
+import io.flutter.plugin.common.PluginRegistry
+import io.flutter.plugin.common.PluginRegistry.PluginRegistrantCallback
+import io.flutter.plugin.common.PluginRegistry.Registrar
+
+/**
+ * PushePlugin
+ * Main class that the developer who uses this plugin interacts with.
+ * Class handles all configuration stuff which developer will do.
+ * Mainly used functions are:
+ *  [PushePlugin.initialize]
+ *  [PushePlugin.debugMode]
+ *  [PushePlugin.registerWith] // both types
+ * @author Mahdi Malvandi
+ */
+@Suppress("SpellCheckingInspection")
+class PushePlugin : FlutterPlugin, ActivityAware {
+
+    override fun onAttachedToEngine(binding: FlutterPluginBinding) {
+        setUpChannel(binding.applicationContext, binding.binaryMessenger)
+    }
+
+    override fun onDetachedFromEngine(binding: FlutterPluginBinding) {
+        PusheLifeCycle.isForeground = false
+    }
+
+    ///// Activity aware functions
+
+    override fun onAttachedToActivity(binding: ActivityPluginBinding) {
+        PusheLifeCycle.isForeground = true
+    }
+
+    override fun onDetachedFromActivityForConfigChanges() {
+        PusheLifeCycle.isForeground = false
+    }
+
+    override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
+        PusheLifeCycle.isForeground = true
+    }
+
+    override fun onDetachedFromActivity() {
+        PusheLifeCycle.isForeground = false
+    }
+
+    companion object {
+
+        /**
+         * In order to be able to use the library callbacks, you need to override the application class and make a custom one.
+         * 1. Create a class called `App` in the root of `android/app/packageName`,
+         * extend [io.flutter.app.FlutterApplication] and implement [io.flutter.plugin.common.PluginRegistry.PluginRegistrantCallback].
+         * 2. Introduce it to the `AndroidManifest.xml` located at, `android/app/src/main/` as `android:name=".App" (or the full packageName of the class)
+         * 3. Add [PushePlugin.initialize] in `onCreate` method of the app class (override it if it does not exist).
+         * 4. Add [PushePlugin.registerWith] and pass the registry in the `registerWith` method (override it if it does not exist).
+         * @param initializer is the instance of your app which follows the extending and implementing rule above.
+         * @param <T> forces the instance to firstly, extend the FlutterApplication (Context) and secondly, implement PluginRegistrantCallback
+         * 5. You're good at native side, you may follow the rest according to https://docs.pushe.co
+         * -- Why is it here? -- To make usage more simple, this class will handle all the stuff and user does not need to know about any other classes.
+        </T> */
+        @JvmStatic
+        fun <T> initialize(initializer: T) where T : Context, T : PluginRegistrantCallback {
+            PusheNotificationListener.initialize(initializer)
+        }
+
+        /**
+         * Registration for v1 embedded flutter projects
+         * Might cause issue with proguard, so you better exclude it in the rules by adding
+         * ```
+         * -keep co.pushe.plus.flutter.** { *; }
+         * ```
+         */
+        @JvmStatic
+        fun registerWith(registry: PluginRegistry) {
+            registerWith(registry.registrarFor("co.pushe.plus.flutter.PushePlugin"))
+        }
+
+        @JvmStatic
+        fun registerWith(registrar: Registrar) {
+            lg("Plugin registered using 'registerWith' static method")
+            setUpChannel(registrar.context(), registrar.messenger())
+        }
+
+        @JvmStatic
+        internal fun setUpChannel(context: Context?, messenger: BinaryMessenger?) {
+            if (context == null || messenger == null) {
+                Log.e("Pushe", "Unhandled exception occurred.\n" +
+                        "Either BinaryMessenger or Android Context is null. So plugin can not set MessageHandlers")
+                return
+            }
+            // Main Method handler
+            val channel = MethodChannel(messenger, "plus.pushe.co/pushe_flutter")
+            val callHandler = PusheChandler(context, messenger)
+            channel.setMethodCallHandler(callHandler)
+
+            // When background was applied
+            val backgroundChannel = MethodChannel(messenger, "plus.pushe.co/pushe_flutter_background")
+            backgroundChannel.setMethodCallHandler(callHandler)
+            PusheNotificationListener.setBackgroundChannel(backgroundChannel)
+        }
+
+        /**
+         * If set to true,
+         * verbose logs will be printed to logcat,
+         * so each step can be tracked and observed.
+         */
+        @JvmStatic
+        var debugMode = false
+    }
+}
---
+++ android/src/main/kotlin/co/pushe/plus/flutter/Utils.kt
@@ -0,0 +1,184 @@
+package co.pushe.plus.flutter
+
+import android.content.Intent
+import android.util.Log
+import co.pushe.plus.flutter.Utils.lg
+import co.pushe.plus.notification.NotificationButtonData
+import co.pushe.plus.notification.NotificationData
+import org.json.JSONArray
+import org.json.JSONException
+import org.json.JSONObject
+
+/**
+ * This static class will do all tasks related to parsing and packing.
+ */
+internal object Pack {
+
+    @JvmStatic
+    fun getNotificationJsonObject(data: NotificationData?): JSONObject? {
+        if (data == null) {
+            return null
+        }
+        val notificationObject = JSONObject()
+        return try {
+            notificationObject.apply {
+                put("title", data.title)
+                put("content", data.content)
+                put("bigTitle", data.bigTitle)
+                put("bigContent", data.bigContent)
+                put("imageUrl", data.imageUrl)
+                put("summary", data.summary)
+                put("iconUrl", data.iconUrl)
+            }
+            try {
+                notificationObject.put("json", JSONObject(data.customContent))
+            } catch (e: Exception) {
+                lg("No json to put as customContent field")
+            }
+            try {
+                notificationObject.put("buttons", getButtonsJsonArray(data.buttons))
+            } catch (e: Exception) {
+                lg("No buttons to put as customContent field")
+            }
+            notificationObject
+        } catch (e: JSONException) {
+            Log.w("Pushe", "Failed to parse notification and convert it to Json.")
+            null
+        }
+    }
+
+    @JvmStatic
+    fun getButtonJsonObject(buttonData: NotificationButtonData?): JSONObject? {
+        if (buttonData == null) {
+            return null
+        }
+        val o = JSONObject()
+        return try {
+            o.run {
+                put("title", buttonData.text)
+                put("icon", buttonData.icon)
+            }
+        } catch (e: JSONException) {
+            Log.w("Pushe", "Failed to parse notification and convert it to Json.")
+            null
+        }
+    }
+
+    @JvmStatic
+    fun getButtonsJsonArray(buttons: List<NotificationButtonData>): JSONArray? {
+        val jA = JSONArray()
+        for (i in buttons) {
+            jA.put(getButtonJsonObject(i))
+        }
+        return jA
+    }
+
+    ///// Background extensions
+
+    @JvmStatic
+    fun getBackgroundNotificationObject(data: NotificationData, type: String, buttonData: NotificationButtonData? = null): JSONObject? {
+        val finalObject = JSONObject()
+        val notificationObject = getNotificationJsonObject(data)
+        if (notificationObject == null) {
+            lg("Failed to get notification object")
+            return null
+        }
+
+        finalObject.put("data", notificationObject)
+
+        if (type == Constants.BUTTON_CLICK && buttonData != null) {
+            val clickedButton = getButtonJsonObject(buttonData)
+            finalObject.put("button", clickedButton)
+        }
+
+        finalObject.put("type", type)
+
+        return finalObject
+    }
+
+    @JvmStatic
+    fun getCustomContent(json: Map<String, Any>): JSONObject {
+        val customContent = JSONObject()
+        customContent.put("json", JSONObject(json))
+        customContent.put("type", Constants.CUSTOM_CONTENT)
+        return customContent
+    }
+
+    @JvmStatic
+    fun getNotificationJsonFromIntent(intent: Intent): JSONObject? {
+        val o = JSONObject()
+        try {
+            val data = intent.getStringExtra("data")
+            o.put("data", JSONObject(data))
+        } catch (e: JSONException) {
+            lg("Failed to parse notification")
+        }
+        return o
+    }
+
+    @JvmStatic
+    fun getCustomContentFromIntent(intent: Intent): JSONObject? {
+        val o = JSONObject()
+        try {
+            val data = intent.getStringExtra("json")
+            o.put("json", JSONObject(data))
+        } catch (e: JSONException) {
+            lg("Failed to parse notification")
+        }
+        return o
+    }
+
+    @JvmStatic
+    fun getButtonJsonFromIntent(intent: Intent): JSONObject? {
+        var o: JSONObject? = JSONObject()
+        try {
+            val button = intent.getStringExtra("button")
+            o = JSONObject(button)
+        } catch (e: JSONException) {
+            lg("Failed to parse notification button")
+        }
+        return o
+    }
+
+    @JvmStatic
+    fun getNotificationAndButtonFromIntent(intent: Intent): JSONObject? {
+        val o = JSONObject()
+        try {
+            o.put("notification", getNotificationJsonFromIntent(intent))
+            o.put("button", getButtonJsonFromIntent(intent))
+        } catch (ignored: JSONException) {
+        }
+        return o
+    }
+}
+
+/**
+ * Normal util class
+ *
+ */
+object Utils {
+
+    /**
+     * Specifies whether the app is in the background or foreground.
+     */
+    fun isAppOnForeground()= PusheLifeCycle.isForeground
+    
+
+    /**
+     * Verbose logger. At first it is not enabled and it will be enabled using `PushePlugin.setDebugMode(true)`
+     */
+    @JvmStatic
+    fun lg(message: String) {
+        if (PushePlugin.debugMode) {
+            Log.d("Pushe", message)
+        }
+    }
+}
+
+/**
+ * Holding a static variable initialized in all lifecycle methods.
+ * If the activity comes up means the user is using and the app is in foreground, otherwise any method calling is happening in the background. 
+ */
+internal object PusheLifeCycle {
+    var isForeground: Boolean = false
+}
